c4ebc5d2649c5a65611f5b66a7f6578cfc9d9923
Tyll Weiß <weiss@sitegeist.de>
2015-11-06 00:51:21 +0100

    [BUGFIX] Call of the semantic-release callback after the log was generated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import concat from 'concat-stream';
+import {StringDecoder} from 'string_decoder';
 import parseGitHubUrl from 'github-url-from-git';
 import changelog from 'conventional-changelog';
 import transform from './transform.js';
@@ -5,11 +7,18 @@ import transform from './transform.js';
 export default (pluginConfig, {pkg}, cb) => {
 	const repository = pkg.repository ? parseGitHubUrl(pkg.repository.url) : null;
 
+	function onStreamData(buffer) {
+		const decoder = new StringDecoder('utf8');
+		const log = decoder.write(buffer);
+
+		cb(null, log);
+	}
+
 	changelog({
 		repository,
 		version: pkg.version,
 		file: false
-	}, cb, null, null, {
+	}, null, null, null, {
 		transform
-	});
+	}).pipe(concat(onStreamData));
 };
